9a70965f46fb5fab30b621b5368116a132cec38b
Jesse Kuang <jkuang@21cn.com>
2018-12-27 15:21:41 +0800

    revise crosscompile for win64

--- build-win64.sh
+++ build-win64.sh
@@ -1,3 +1,3 @@
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 PKG_CONFIG_LIBDIR=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc go build -o bin/rebot.exe rebot/main.go
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 PKG_CONFIG_LIBDIR=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig CXX=x86_64-w64-mingw32-g++ CC=x86_64-w64-mingw32-gcc go build -o bin/rebot.exe rebot/main.go
 x86_64-w64-mingw32-strip bin/rebot.exe
 cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/SDL2.dll bin
